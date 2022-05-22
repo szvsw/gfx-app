a1135668ce8379bc899cf4cf61c17bfc2839b7ad
voidstar <mail@samwolk.info>
2022-05-22 06:43:28 -0400

    update menubar

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,16 +21,23 @@ export const Header: React.FC<Props> = (props) => {
 
       <Box sx={{p: "2rem"}}>
 
-        <Stack direction="row" sx={{justifyContent: "space-between"}}>
-          <Typography variant="h2">Interactive Graphic Statics</Typography>
+        <Stack direction="row" sx={{alignItems: "center", justifyContent: "space-between"}}>
+          <Link href="/">
+          <Button variant="text">
+            <Typography variant="h2">Interactive Graphic Statics</Typography>
+          </Button>
+          </Link>
+          <Box>
           <ButtonGroup variant="outlined">
-            <Link href="/">
-              <Button>Home</Button>
+            <Link href="/hookes-law">
+              <Button >Hooke's Law</Button>
             </Link>
             <Link href="/counterweighted-deck">
               <Button >Counterweighted Cable-Stayed Deck</Button>
             </Link>
           </ButtonGroup>
+
+          </Box>
         </Stack>
 
         {props.children}
---
+++ pages/hookes-law.tsx
@@ -0,0 +1,8 @@
+import Typography from "@mui/material/Typography"
+export default function HookesLaw() {
+	return (
+	<Typography>
+    Coming Soon.
+		</Typography>
+	)
+}
